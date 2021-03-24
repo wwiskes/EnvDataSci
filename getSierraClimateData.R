f8886cf715f83c7b75d0b86775bd3bd4e0c5e83e
JERRY D DAVIS <32618011+spedunkler@users.noreply.github.com>
2021-03-24 14:56:23 -0700

    Most of the draft done for four sections (introR, tidyverse, visualization, spatial), just need exercises.

--- getSierraClimateData.R
+++ getSierraClimateData.R
@@ -6,11 +6,18 @@
 #    and write out as a new csv
 library(sf)
 library(tidyverse)
-sierraStations <- read_csv("sierraStations.csv")
-normals <- read_csv("2289543.csv")
+sierraStations <- read_csv("data/sierraStations.csv")
+normals <- read_csv("data/2289543.csv")
 #Add counties
-co <- st_read("CA_counties.shp") %>%
+co <- st_read("data/CA_counties.shp") %>%
   select(COUNTY=NAME)
+
+sierraData <- right_join(sierraStations,normals,by="STATION") %>%
+  filter(!is.na(STATION_NA)) %>% dplyr::select(-STATION_NA) %>%
+  mutate(PRECIPITATION = `MLY-PRCP-NORMAL`,
+         TEMPERATURE = `MLY-TAVG-NORMAL`) %>%
+  dplyr::select(NAME, ELEVATION, LATITUDE, LONGITUDE, PRECIPITATION, TEMPERATURE)
+write_csv(sierraData, "data/sierraData.csv")
 
 sierraFeb <- right_join(sierraStations,normals,by="STATION") %>%
   filter(!is.na(STATION_NA)) %>% dplyr::select(-STATION_NA) %>%
@@ -25,4 +32,4 @@ sierraFeb <- st_as_sf(sierraFeb, coords=c("LONGITUDE","LATITUDE"), remove=FALSE,
   st_drop_geometry() %>%
   dplyr::select(STATION_NAME, COUNTY, ELEVATION, LATITUDE, LONGITUDE, PRECIPITATION, TEMPERATURE)
 
-write_csv(sierraFeb, "sierraFeb.csv")
+write_csv(sierraFeb, "data/sierraFeb.csv")
